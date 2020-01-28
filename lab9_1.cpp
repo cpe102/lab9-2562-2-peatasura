47b9c986ab4515c16fd3f4d028c53d646f98f183
peatasura <keenphantom.2013@gmail.com>
2020-01-28 16:32:21 +0700

    rt

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,28 +1,52 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+int main()
+{
+	int count[5] = {};
+	char grade; //Declare array count for counting A,B,C,D,F and initialize all element = 0
 	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
+	do
+	{
 		cout << "Student [" << "]:";
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
+		if(grade == 'A')
+			{
+				count[0]++ ;
+			}  // if grade is A
 			//Do something
-		}else if(true) // if grade is B
+		else if(grade == 'B') 
+		{
+			count[1]++ ;
+		} // if grade is B
 			//Do something
 		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
+		else if(grade == 'C') 
+		{
+			count[2]++ ;
+		}
+		else if(grade == 'D') 
+		{
+			count[3]++ ;
+		}
+		else if(grade == 'F') 
+		{
+			count[4]++ ;
+		}
+		else{ // grade is wrong input
+			//Do somethingc
+			cout << "Wrong input. Please input again.";
 		} 
-	}while(true);
+	}while(grade != '0' );
+
 	
 	
 	cout << "In total ? students.";
 	cout << "A = " << count[0] <<",";
 	cout << "B = " << count[1] <<",";	
 	//	and so on ... for grade = C, D, F	
-	
-	}
 	return 0;
-}
+	}
+	
+
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,25 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main()
+{
+    //cout << "-------------------- SOTUS ---------------------";
+    ifstream source;
+    ofstream cheerbook_copy("C:\\Users\\PrayatZ\\Desktop\\lab9\\lab9-2562-2-peatasura\\cheerbook_copy.txt");
+    source.open("C:\\Users\\PrayatZ\\Desktop\\lab9\\lab9-2562-2-peatasura\\cheerbook.txt");
+    cheerbook_copy << "-------------------- SOTUS ---------------------\n";
+    string textline;
+    //bool cheerbook_copy;
+   // cheerbook_copy = getline(source,textline);
+    while(getline(source,textline))
+    {
+        cheerbook_copy << textline << '\n';
+    }
+    source.close();
+    cheerbook_copy << "-------------------- SOTUS ---------------------";
+    return 0;
+}
