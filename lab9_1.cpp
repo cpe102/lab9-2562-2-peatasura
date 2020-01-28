6f49d91c57cea178dbe2de5115bd4138df0b6e52
peatasura <keenphantom.2013@gmail.com>
2020-01-28 16:36:15 +0700

    Finn

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,52 +1,57 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
-int main()
-{
+int main(){
 	int count[5] = {};
-	char grade; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do
-	{
-		cout << "Student [" << "]:";
+	char grade;
+	int i=1; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
+	do{
+		cout << "Student [" << i << "]:";
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(grade == 'A')
-			{
-				count[0]++ ;
-			}  // if grade is A
-			//Do something
-		else if(grade == 'B') 
+		if(grade=='A') 
 		{
-			count[1]++ ;
-		} // if grade is B
+			count[0]++;// if grade is A
+			//Do something
+		}else if(grade=='B')
+		{
+			count[1]++;
+		} 
+		else if(grade=='C')
+		{
+			count[2]++;
+		} 
+		else if(grade=='D')
+		{
+			count[3]++;
+		} 
+		else if(grade=='F')
+		{
+			count[4]++;
+		}
+		else if(grade=='0') 
+		{
+			i--;
+			break;// if grade is B
 			//Do something
 		//and so on ... for grade = C, D, F	
-		else if(grade == 'C') 
-		{
-			count[2]++ ;
-		}
-		else if(grade == 'D') 
-		{
-			count[3]++ ;
-		}
-		else if(grade == 'F') 
-		{
-			count[4]++ ;
-		}
-		else{ // grade is wrong input
-			//Do somethingc
-			cout << "Wrong input. Please input again.";
+		}else{ // grade is wrong input
+			//Do somethingco
+			cout << "Wrong input.Please input again.\n";
+			i--;
 		} 
-	}while(grade != '0' );
-
+		i++;
+	}while(i>0);
 	
 	
-	cout << "In total ? students.";
+	cout << "In total " << i <<" students.";
 	cout << "A = " << count[0] <<",";
 	cout << "B = " << count[1] <<",";	
 	//	and so on ... for grade = C, D, F	
-	return 0;
-	}
+	cout << "C = " << count[2] <<",";
+	cout << "D = " << count[3] <<",";
+	cout << "F = " << count[4] <<",";
 	
-
+	
+	return 0;
+}
